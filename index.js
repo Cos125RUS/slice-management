2c5a9ebe71d9fffb5a9432c4592e1475c88e1f3d
Valerii_Panov <val250640@mail.ru>
2025-02-19 07:42:07 +1000

    поменял экспорты в index.js

--- index.js
+++ index.js
@@ -1,59 +1,14 @@
-import CaseCreator from './lib/cases/CaseCreator';
-import {loadingApiCases} from './lib/cases/loadingApiCases';
-import ConverterChain from './lib/converters/ConverterChain';
-import {snakeCaseToCamelCaseConverter} from './lib/converters/snakeCaseToCamelCaseConverter';
-import {sortByIdConverter} from './lib/converters/sortByIdConverter';
-import {defaultLoader} from './lib/loaders/defaultLoader';
-import {getNewLoader} from './lib/loaders/getNewLoader';
-import {getStubLoader,getSimpleLoader,getOptionalLoader,getUpdater,getPublisher,getDeleter,getResourceInteraction} from './lib/loaders/requester';
-import {stubLoader} from './lib/loaders/stubLoader';
-import CustomSlice from './lib/slice-manager/CustomSlice';
-import {getTtlPersistTransform} from './lib/slice-manager/getTtlPersistTransform';
-import PersistSlice from './lib/slice-manager/PersistSlice';
-import PersistSliceManager from './lib/slice-manager/PersistSliceManager';
-import SliceManager from './lib/slice-manager/SliceManager';
-
-
-const CaseCreator = CaseCreator;
-const loadingApiCases = loadingApiCases;
-const ConverterChain = ConverterChain;
-const snakeCaseToCamelCaseConverter = snakeCaseToCamelCaseConverter;
-const sortByIdConverter = sortByIdConverter;
-const defaultLoader = defaultLoader;
-const getNewLoader = getNewLoader;
-const getStubLoader = getStubLoader;
-const getSimpleLoader = getSimpleLoader;
-const getOptionalLoader = getOptionalLoader;
-const getUpdater = getUpdater;
-const getPublisher = getPublisher;
-const getDeleter = getDeleter;
-const getResourceInteraction = getResourceInteraction;
-const stubLoader = stubLoader;
-const CustomSlice = CustomSlice;
-const getTtlPersistTransform = getTtlPersistTransform;
-const PersistSlice = PersistSlice;
-const PersistSliceManager = PersistSliceManager;
-const SliceManager = SliceManager;
-
-export {
-    CaseCreator,
-    loadingApiCases,
-    ConverterChain,
-    snakeCaseToCamelCaseConverter,
-    sortByIdConverter,
-    defaultLoader,
-    getNewLoader,
-    getStubLoader,
-    getSimpleLoader,
-    getOptionalLoader,
-    getUpdater,
-    getPublisher,
-    getDeleter,
-    getResourceInteraction,
-    stubLoader,
-    CustomSlice,
-    getTtlPersistTransform,
-    PersistSlice,
-    PersistSliceManager,
-    SliceManager,
-};
+export CaseCreator from './lib/cases/CaseCreator';
+export {loadingApiCases} from './lib/cases/loadingApiCases';
+export ConverterChain from './lib/converters/ConverterChain';
+export {snakeCaseToCamelCaseConverter} from './lib/converters/snakeCaseToCamelCaseConverter';
+export {sortByIdConverter} from './lib/converters/sortByIdConverter';
+export {defaultLoader} from './lib/loaders/defaultLoader';
+export {getNewLoader} from './lib/loaders/getNewLoader';
+export {getStubLoader,getSimpleLoader,getOptionalLoader,getUpdater,getPublisher,getDeleter,getResourceInteraction} from './lib/loaders/requester';
+export {stubLoader} from './lib/loaders/stubLoader';
+export CustomSlice from './lib/slice-manager/CustomSlice';
+export {getTtlPersistTransform} from './lib/slice-manager/getTtlPersistTransform';
+export PersistSlice from './lib/slice-manager/PersistSlice';
+export PersistSliceManager from './lib/slice-manager/PersistSliceManager';
+export SliceManager from './lib/slice-manager/SliceManager';
